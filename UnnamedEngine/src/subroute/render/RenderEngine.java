73d7996098d012198a70a6612dfa44bfe3e6e6f6
Silversmith <alexadranly@gmail.com>
2014-03-24 14:11:30 -0700

    this

---
+++ UnnamedEngine/src/subroute/render/RenderEngine.java
@@ -0,0 +1,320 @@
+package subroute.render;
+
+import java.nio.FloatBuffer;
+
+import org.lwjgl.BufferUtils;
+import org.lwjgl.input.Keyboard;
+import org.lwjgl.opengl.GL11;
+import org.lwjgl.util.vector.Vector3f;
+
+import subroute.Game;
+import subroute.audio.AudioEngine;
+import subroute.block.Block;
+import subroute.entity.Entity;
+import subroute.gui.GUIRenderer;
+import subroute.input.KeyboardReader;
+import subroute.menu.MainMenu;
+import subroute.phys.util.AABB;
+import subroute.player.Player;
+import subroute.render.camera.Camera;
+import subroute.texture.TextureRegistry;
+import subroute.world.WorldLoader;
+import subroute.world.storage.WorldStorage;
+
+
+public class RenderEngine {
+	public static RenderEngine instance;
+	public static TextureRegistry textureMap;
+	public static FontRenderer fontRenderer;
+	public static RenderBlocks renderBlocks = new RenderBlocks();
+	public static AABB bbox1, bbox2, bbox3,bbox4;
+
+	public RenderEngine(){
+		instance = this;
+		fontRenderer = new FontRenderer();
+		textureMap = new TextureRegistry();
+		textureMap.loadBaseTextures();
+		new GUIRenderer();
+
+		new Camera(new Player());
+
+		this.initLighting();
+		GUIRenderer.setCurrentGUI(new MainMenu());
+		AudioEngine.instance().loadSound();
+	}
+	AABB playerBounds;
+	private boolean dgen = false;
+	private final Vector3f lpos = new Vector3f(0,0,0);
+	float time;
+	WorldStorage wldstor;
+	private boolean bool = false, bool2 = false, bool3 = false;
+	public void renderLoop(){
+
+
+		GL11.glClearColor(0f,0f,0f,1f);
+		GL11.glClear(GL11.GL_DEPTH_BUFFER_BIT|GL11.GL_COLOR_BUFFER_BIT);
+
+		GL11.glPushMatrix();
+
+		if(wldstor!=null){
+			this.wldstor.loadNextRenderer();
+			Camera.camTransform();
+
+			if(KeyboardReader.keysts[Keyboard.KEY_UP]){
+				time += 0.1d*Game.getDelta();
+			}
+			if(KeyboardReader.keysts[Keyboard.KEY_DOWN]){
+				time -= 0.1d*Game.getDelta();
+			}
+			if(KeyboardReader.keysts[Keyboard.KEY_F5]!=bool3&&KeyboardReader.keysts[Keyboard.KEY_F5]==false){
+				Camera.setThirdPerson(!Camera.isThirdPerson());
+			}
+			bool3 = KeyboardReader.keysts[Keyboard.KEY_F5];
+			if(KeyboardReader.keysts[Keyboard.KEY_P]!=bool2&&KeyboardReader.keysts[Keyboard.KEY_P]==false){
+				AudioEngine.instance().playSound();
+			}
+			bool2 = KeyboardReader.keysts[Keyboard.KEY_P];
+			if(KeyboardReader.keysts[Keyboard.KEY_R]!=bool&&KeyboardReader.keysts[Keyboard.KEY_R]==false){
+				dgen = !dgen;
+				System.out.println("Dynamic segment loading toggled "+(dgen?"on":"off"));
+			}
+			bool=KeyboardReader.keysts[Keyboard.KEY_R];
+
+			lpos.y=1;
+			if(time > 360){
+				time=0;
+			}else{
+				Game.instance();
+				time+=Game.getDelta()/1000f;
+			}
+			double p = Math.sin(Math.toRadians(time))*1000;
+			double p2 = Math.cos(Math.toRadians(time))*1000;
+			double p3 = Math.sin(Math.toRadians(time+180))*1000;
+			double p4 = Math.cos(Math.toRadians(time+180))*1000;
+			FloatBuffer pos = BufferUtils.createFloatBuffer(4).put(new float[]{1f,(float)p3+256,(float)p4,0.2f});
+			FloatBuffer pos2 = BufferUtils.createFloatBuffer(4).put(new float[]{1f,(float)p+256,(float)p2,0.2f});
+			GL11.glLight(GL11.GL_LIGHT0, GL11.GL_POSITION, (FloatBuffer) pos.flip());
+			GL11.glLight(GL11.GL_LIGHT1, GL11.GL_POSITION, (FloatBuffer) pos2.flip());
+
+			FloatBuffer fcolor = (FloatBuffer) BufferUtils.createFloatBuffer(4).put(new float[]{0.5f,0.5f,0.5f,1f}).flip();
+			GL11.glFogf(GL11.GL_FOG_DENSITY, 100f);
+			GL11.glFog(GL11.GL_FOG_COLOR, fcolor);
+			GL11.glFogi(GL11.GL_FOG_END, 90);
+			GL11.glFogi(GL11.GL_FOG_START, 80);
+			GL11.glFogi(GL11.GL_FOG_MODE, GL11.GL_LINEAR);
+			GL11.glPointSize(100f);
+			GL11.glBegin(GL11.GL_POINTS);
+			GL11.glColor4f(1f, 1f, 0f, 1f);
+			GL11.glVertex3d(0, p+256, p2);
+			GL11.glColor4f(1f, 1f, 1f, 1f);
+			GL11.glVertex3d(0, p3+256, p4);
+			GL11.glEnd();
+
+			GL11.glEnable(GL11.GL_LIGHTING);
+			if(time<180){
+				GL11.glEnable(GL11.GL_LIGHT1);
+				GL11.glDisable(GL11.GL_LIGHT0);
+			}else{
+				GL11.glEnable(GL11.GL_LIGHT0);
+				GL11.glDisable(GL11.GL_LIGHT1);
+			}
+			GL11.glEnable(GL11.GL_CULL_FACE);
+			GL11.glFrontFace(GL11.GL_CW);
+
+			GL11.glColor4f(1f, 1f, 1f, 1f);
+			GL11.glDisable(GL11.GL_CULL_FACE);
+			GL11.glDisable(GL11.GL_LIGHTING);
+//			GL11.glDisable(GL11.GL_FOG);
+			GL11.glPointSize(10f);
+			GL11.glBegin(GL11.GL_POINTS);
+			Entity ent = Camera.getBoundEntity();
+			GL11.glVertex3d(ent.getXPos()+ent.xVel*(Game.getDelta()/1000d),ent.getYPos()+ent.yVel*(Game.getDelta()/1000d),ent.getZPos()+ent.zVel*(Game.getDelta()/1000d));
+			GL11.glEnd();
+
+
+			AABB bbox =  Camera.getBoundEntity().getBoundingBox();
+			playerBounds = bbox;
+			bbox.renderDebugBox();
+
+			//does not work on negative x side
+			//does not work on negative z
+			//fix negative problems, then send information to entity to respond to collision
+			if((ent.xPos >= 0)&&(ent.zPos >=0)){
+
+			int id = WorldStorage.getInstance().getBlockIdAt((int)ent.xPos,(int)ent.yPos+1, (int)(ent.zPos+1));
+			int id2 = WorldStorage.getInstance().getBlockIdAt((int)ent.xPos,(int)ent.yPos+1, (int)(ent.zPos-1));
+			int id3 = WorldStorage.getInstance().getBlockIdAt((int)(ent.xPos+1),(int)ent.yPos+1, (int)ent.zPos);
+			int id4 = WorldStorage.getInstance().getBlockIdAt((int)(ent.xPos-1),(int)ent.yPos+1, (int)ent.zPos);
+
+			System.out.print(id + " ");
+			System.out.print(id2 + " ");
+			System.out.print(id3+ " ");
+			System.out.println(id4);
+
+			if((id !=0)){
+				bbox1 = Block.blocks[id].getBoudingBox(WorldStorage.getInstance(),(int)(ent.xPos),(int)(ent.yPos), (int)Math.floor(ent.zPos+1));
+				bbox1.renderDebugBox();
+			}
+			if((id2 !=0)){
+				bbox2 = Block.blocks[id2].getBoudingBox(WorldStorage.getInstance(),(int)(ent.xPos),(int)(ent.yPos), (int)Math.floor(ent.zPos-1));
+				bbox2.renderDebugBox();
+			}
+			if((id3 !=0)){
+				bbox3 = Block.blocks[id3].getBoudingBox(WorldStorage.getInstance(),(int)Math.floor(ent.xPos+1),(int)(ent.yPos), (int)(ent.zPos));
+				bbox3.renderDebugBox();
+			}
+			if((id4 !=0)){
+				bbox4 = Block.blocks[id4].getBoudingBox(WorldStorage.getInstance(),(int)Math.floor(ent.xPos-1),(int)(ent.yPos), (int)(ent.zPos));
+				bbox4.renderDebugBox();
+			}
+
+			ent.getBoundingBox();
+			}
+
+			if((ent.xPos < 0)&&(ent.zPos <0)){
+
+
+				int id = WorldStorage.getInstance().getBlockIdAt((int)ent.xPos-1,(int)ent.yPos+1, (int)(ent.zPos));
+				int id2 = WorldStorage.getInstance().getBlockIdAt((int)ent.xPos-1,(int)ent.yPos+1, (int)(ent.zPos-2));
+				int id3 = WorldStorage.getInstance().getBlockIdAt((int)(ent.xPos),(int)ent.yPos+1, (int)ent.zPos-1);
+				int id4 = WorldStorage.getInstance().getBlockIdAt((int)(ent.xPos-2),(int)ent.yPos+1, (int)ent.zPos-1);
+
+				System.out.print(id + " ");
+				System.out.print(id2 + " ");
+				System.out.print(id3+ " ");
+				System.out.println(id4);
+
+				if((id !=0)){
+					bbox1 = Block.blocks[id].getBoudingBox(WorldStorage.getInstance(),(int)(ent.xPos-1),(int)(ent.yPos), (int)Math.ceil(ent.zPos));
+					bbox1.renderDebugBox();
+				}
+				if((id2 !=0)){
+					bbox2 = Block.blocks[id2].getBoudingBox(WorldStorage.getInstance(),(int)(ent.xPos-1),(int)(ent.yPos), (int)Math.ceil(ent.zPos-2));
+					bbox2.renderDebugBox();
+				}
+				if((id3 !=0)){
+					bbox3 = Block.blocks[id3].getBoudingBox(WorldStorage.getInstance(),(int)Math.ceil(ent.xPos),(int)(ent.yPos), (int)(ent.zPos-1));
+					bbox3.renderDebugBox();
+				}
+				if((id4 !=0)){
+					bbox4 = Block.blocks[id4].getBoudingBox(WorldStorage.getInstance(),(int)Math.ceil(ent.xPos-2),(int)(ent.yPos), (int)(ent.zPos-1));
+					bbox4.renderDebugBox();
+				}
+				}
+
+			if((ent.xPos >= 0)&&(ent.zPos < 0)){
+				int id = WorldStorage.getInstance().getBlockIdAt((int)ent.xPos,(int)ent.yPos+1, (int)(ent.zPos));
+				int id2 = WorldStorage.getInstance().getBlockIdAt((int)ent.xPos,(int)ent.yPos+1, (int)(ent.zPos-2));
+				int id3 = WorldStorage.getInstance().getBlockIdAt((int)(ent.xPos+1),(int)ent.yPos+1, (int)ent.zPos-1);
+				int id4 = WorldStorage.getInstance().getBlockIdAt((int)(ent.xPos-1),(int)ent.yPos+1, (int)ent.zPos-1);
+
+				System.out.print(id + " ");
+				System.out.print(id2 + " ");
+				System.out.print(id3+ " ");
+				System.out.println(id4);
+
+				if((id !=0)){
+					bbox1 = Block.blocks[id].getBoudingBox(WorldStorage.getInstance(),(int)(ent.xPos),(int)(ent.yPos), (int)Math.ceil(ent.zPos));
+					bbox1.renderDebugBox();
+
+				}
+				if((id2 !=0)){
+					bbox2 = Block.blocks[id2].getBoudingBox(WorldStorage.getInstance(),(int)(ent.xPos),(int)(ent.yPos), (int)Math.ceil(ent.zPos-2));
+					bbox2.renderDebugBox();
+				}
+				if((id3 !=0)){
+					bbox3 = Block.blocks[id3].getBoudingBox(WorldStorage.getInstance(),(int)Math.floor(ent.xPos+1),(int)(ent.yPos), (int)(ent.zPos-1));
+					bbox3.renderDebugBox();
+				}
+				if((id4 !=0)){
+					bbox4 = Block.blocks[id4].getBoudingBox(WorldStorage.getInstance(),(int)Math.floor(ent.xPos-1),(int)(ent.yPos), (int)(ent.zPos-1));
+					bbox4.renderDebugBox();
+				}
+				}
+
+			if((ent.xPos < 0)&&(ent.zPos >=0)){
+				int id = WorldStorage.getInstance().getBlockIdAt((int)ent.xPos-1,(int)ent.yPos+1, (int)(ent.zPos+1));
+				int id2 = WorldStorage.getInstance().getBlockIdAt((int)ent.xPos-1,(int)ent.yPos+1, (int)(ent.zPos-1));
+				int id3 = WorldStorage.getInstance().getBlockIdAt((int)(ent.xPos),(int)ent.yPos+1, (int)ent.zPos);
+				int id4 = WorldStorage.getInstance().getBlockIdAt((int)(ent.xPos-2),(int)ent.yPos+1, (int)ent.zPos);
+
+				System.out.print(id + " ");
+				System.out.print(id2 + " ");
+				System.out.print(id3+ " ");
+				System.out.println(id4);
+
+				if((id !=0)){
+					bbox1 = Block.blocks[id].getBoudingBox(WorldStorage.getInstance(),(int)(ent.xPos-1),(int)(ent.yPos), (int)Math.floor(ent.zPos+1));
+					bbox1.renderDebugBox();
+				}
+				if((id2 !=0)){
+					bbox2 = Block.blocks[id2].getBoudingBox(WorldStorage.getInstance(),(int)(ent.xPos-1),(int)(ent.yPos), (int)Math.floor(ent.zPos-1));
+					bbox2.renderDebugBox();
+				}
+				if((id3 !=0)){
+					bbox3 = Block.blocks[id3].getBoudingBox(WorldStorage.getInstance(),(int)Math.ceil(ent.xPos),(int)(ent.yPos), (int)(ent.zPos));
+					bbox3.renderDebugBox();
+				}
+				if((id4 !=0)){
+					bbox4 = Block.blocks[id4].getBoudingBox(WorldStorage.getInstance(),(int)Math.ceil(ent.xPos-2),(int)(ent.yPos), (int)(ent.zPos));
+					bbox4.renderDebugBox();
+				}
+				}
+
+			GL11.glEnd();
+
+			renderBlocks.renderBlockOutline();
+			GL11.glEnable(GL11.GL_CULL_FACE);
+			GL11.glEnable(GL11.GL_LIGHTING);
+//			GL11.glEnable(GL11.GL_FOG);
+			GL11.glPushMatrix();
+			renderBlocks.render();
+			GL11.glPopMatrix();
+
+			GL11.glFrontFace(GL11.GL_CCW);
+			GL11.glDisable(GL11.GL_CULL_FACE);
+			GL11.glDisable(GL11.GL_LIGHTING);
+			GL11.glDisable(GL11.GL_FOG);
+			if(dgen){
+				renderBlocks.checkForSegGen();
+			}
+		}
+		GUIRenderer.renderGUI();
+		GL11.glPopMatrix();
+
+	}
+
+	public void initLighting(){
+		FloatBuffer gamb = (FloatBuffer) BufferUtils.createFloatBuffer(4).put(new float[]{1f,1f,1f,1f}).flip();
+		FloatBuffer amb = (FloatBuffer) BufferUtils.createFloatBuffer(4).put(new float[]{1f,1f,1f,1}).flip();
+		FloatBuffer amb2 = (FloatBuffer) BufferUtils.createFloatBuffer(4).put(new float[]{1f,1f,1f,1f}).flip();
+		FloatBuffer diff = (FloatBuffer) BufferUtils.createFloatBuffer(4).put(new float[]{0.2f,0.2f,0.2f,0.5f}).flip();
+		FloatBuffer diff2 = (FloatBuffer) BufferUtils.createFloatBuffer(4).put(new float[]{1f,1f,1f,0.5f}).flip();
+		FloatBuffer spec = (FloatBuffer) BufferUtils.createFloatBuffer(4).put(new float[]{0f,0f,0f,0f}).flip();
+		GL11.glLight(GL11.GL_LIGHT0, GL11.GL_AMBIENT, amb);
+		GL11.glLight(GL11.GL_LIGHT0, GL11.GL_DIFFUSE, diff);
+		GL11.glLight(GL11.GL_LIGHT1, GL11.GL_DIFFUSE, diff2);
+		GL11.glLight(GL11.GL_LIGHT1, GL11.GL_AMBIENT, amb2);
+		GL11.glLight(GL11.GL_LIGHT1, GL11.GL_SPECULAR, spec);
+		GL11.glLight(GL11.GL_LIGHT0, GL11.GL_SPECULAR, spec);
+		GL11.glLightModel(GL11.GL_LIGHT_MODEL_AMBIENT, gamb);
+		GL11.glMaterialf(GL11.GL_FRONT, GL11.GL_SHININESS, 0f);
+	}
+	public void renderDebugAxis(){
+		GL11.glLineWidth(5f);
+		GL11.glBegin(GL11.GL_LINES);
+		GL11.glColor4f(1f, 0f, 0f, 1f);
+		GL11.glVertex3i(0, 0, 0);
+		GL11.glVertex3i(10, 0, 0);
+		GL11.glColor4f(0f, 1f, 0f, 1f);
+		GL11.glVertex3i(0, 0, 0);
+		GL11.glVertex3i(0, 10, 0);
+		GL11.glColor4f(0f, 0f, 1f, 1f);
+		GL11.glVertex3i(0, 0, 0);
+		GL11.glVertex3i(0, 0, 10);
+		GL11.glEnd();
+	}
+	public void loadWorld(){
+		wldstor = new WorldStorage(new WorldLoader());
+	}
+}
---
+++ UnnamedEngine/src/subroute/world/storage/WorldStorage.java
@@ -0,0 +1,248 @@
+package subroute.world.storage;
+
+import subroute.render.RenderEngine;
+import subroute.render.Renderer;
+import subroute.world.WorldLoader;
+import subroute.world.segment.Segment;
+
+
+public class WorldStorage implements IWorldAccess{
+
+	private int MAX_SEGMENTS_RADIUS = 10;
+	private SegLoader segLoad = new SegLoader();
+	private boolean needsUpdate = false;
+	private boolean loaded = false;
+	private static WorldStorage instance;
+	private static WorldLoader loader;
+
+	public WorldStorage(WorldLoader wl){
+		if(instance == null){
+			loader = wl;
+			instance = this;
+			this.generateSquare(wl, this.MAX_SEGMENTS_RADIUS, 0, 0);
+		}
+	}
+	private void generateSquare(WorldLoader wl, int radius, int sx, int sy){
+		this.generateSquare(wl, radius, sx, sy, 1);
+	}
+	private void generateSquare(WorldLoader wl, int radius, int startx, int starty, int level){
+		System.out.println("Attempting square gen");
+		if(level<=radius){
+			int x = startx;
+			int y = starty;
+			for(int i = 0; i < (level*2)-1; i++){
+				System.out.println("Generating segment: "+x+","+y);
+				segLoad.addSegment(wl.generateSegment(x,y).setWorld(this));
+				x--;
+			}
+			for(int i = 0; i < (level*2)-1; i++){
+				System.out.println("Generating segment: "+x+","+y);
+				segLoad.addSegment(wl.generateSegment(x,y).setWorld(this));
+				y--;
+			}
+			for(int i = 0; i < (level*2)-1; i++){
+				System.out.println("Generating segment: "+x+","+y);
+				segLoad.addSegment(wl.generateSegment(x,y).setWorld(this));
+				x++;
+			}
+			for(int i = 0; i < (level*2)-1; i++){
+				System.out.println("Generating segment: "+x+","+y);
+				segLoad.addSegment(wl.generateSegment(x,y).setWorld(this));
+				y++;
+			}
+			level++;
+			startx++;
+			starty++;
+			this.generateSquare(wl, radius, startx, starty, level);
+		}else{
+			return;
+		}
+	}
+
+	int advx,advy,sx=0,sy=0,level=1,rot=0;
+
+	public boolean isLoaded(){
+		return loaded;
+	}
+	double inc = 0;
+	private boolean done = false;
+	public void loadNextRenderer(){
+		if(!done){
+			this.updateNeeded();
+			int r = (level*2)-1;
+			if(level>this.MAX_SEGMENTS_RADIUS){
+				done= true;
+				return;
+			}
+			switch(rot){
+				case 0:
+					renderInSegment(sx,sy);
+					sx--;
+					if(sx<-(level-1)){
+						rot++;
+					}
+					break;
+				case 1:
+					renderInSegment(sx,sy);
+					sy--;
+					if(sy<-(level-1)){
+						rot++;
+					}
+					break;
+				case 2:
+					renderInSegment(sx,sy);
+					sx++;
+					if(sx>=level-1){
+						rot++;
+					}
+					break;
+				case 3:
+					renderInSegment(sx,sy);
+					sy++;
+					if(sy>=level-1){
+						rot=0;
+						level++;
+						sx = level-1;
+						sy = level-1;
+					}
+					break;
+			}
+		}
+	}
+	private void renderInSegment(int x, int y){
+		Segment seg = segLoad.getSegmentAt(x, y);
+		if(seg != null&&seg.getRenderer()==null){
+			Renderer render = new Renderer(seg);
+			render.updateRenderable();
+			RenderEngine.renderBlocks.addRenderer(render);
+			render.setUpdated(true);
+//			System.out.println("Sucessfully loaded segment at x:"+(lastx)+" y:"+(lasty));
+		}
+	}
+
+	@Override
+	public int getBlockIdAt(int x, int y, int z) {
+		int sx = (int)Math.floor((x)/16d);
+		int sy = (int)Math.floor((z)/16d);
+		Segment seg = segLoad.getSegmentAt(sx, sy);
+		if(seg!=null){
+			return seg.getBlockIdAt(x, y, z);
+		}
+		return 0;
+	}
+
+	public static WorldStorage getInstance(){
+		return instance;
+	}
+
+
+	@Override
+	public boolean setBlockAt(int x, int y, int z, int id) {
+		this.updateNeeded();
+		int sx = (int)Math.floor((x)/16d);
+		int sy = (int)Math.floor((z)/16d);
+		Segment seg = segLoad.getSegmentAt(sx, sy);
+		if(seg!=null){
+			boolean success = seg.setBlockAt(x, y, z, id);
+			if(success){
+				this.updateAdjSegments(sx, sy);
+			}
+			return success;
+		}
+		return false;
+	}
+	public static boolean check = true;
+	private int cx,cy;
+	public void needCheck(int x, int y){
+		cx = x;
+		cy = y;
+		check = false;
+	}
+	int lastx,lasty;
+	public void checkGenRadius(){
+		if(!check){
+		check = false;
+		this.updateNeeded();
+		if(inc >= this.MAX_SEGMENTS_RADIUS*360){
+			inc = 0;
+			check = true;
+			this.updated();
+			return;
+		}
+		int x = -1,y = -1;
+		while(true){
+			x = (int)(Math.sin(Math.toRadians(inc))*(int)(inc/360));
+			y = (int)(Math.cos(Math.toRadians(inc))*(int)(inc/360));
+			inc+=0.5;
+			if(x==lastx&&y==lasty){
+				continue;
+			}else{
+				lastx = x;
+				lasty = y;
+				break;
+			}
+		}
+		if(segLoad.getSegmentAt(x+cx, y+cy)==null){
+			Segment seg;
+			segLoad.addSegment(seg = WorldStorage.loader.generateSegment(x+cx, y+cy).setWorld(this));
+			Renderer render = new Renderer(seg);
+			seg.renderBlocks(render);
+			RenderEngine.renderBlocks.addRenderer(render);
+//			render.setUpdated(true);
+		}
+		}
+	}
+
+	@Override
+	public void updateNeeded() {
+		this.needsUpdate = true;
+	}
+
+	@Override
+	public boolean getIsUpdateNeeded() {
+		return this.needsUpdate;
+	}
+
+	private void updated(){
+		this.needsUpdate = false;
+	}
+
+	@Override
+	public boolean blockExistsAt(int x, int y, int z) {
+		return this.getBlockIdAt(x, y, z)!=0;
+	}
+
+	public void updateAdjSegments(int x, int y){
+		Segment seg;
+		if((seg=segLoad.getSegmentAt(x+1, y))!=null){
+			seg.getRenderer().setUpdated(true);
+		}
+		if((seg=segLoad.getSegmentAt(x, y+1))!=null){
+			seg.getRenderer().setUpdated(true);
+		}
+		if((seg=segLoad.getSegmentAt(x-1, y))!=null){
+			seg.getRenderer().setUpdated(true);
+		}
+		if((seg=segLoad.getSegmentAt(x, y-1))!=null){
+			seg.getRenderer().setUpdated(true);
+		}
+	}
+
+	public int[] getNextBlockInColumn(boolean down, int x, int z, int height){
+		int y = height;
+		int id = -1;
+		do{
+			if(!(y<=0||y>=1024)){
+				id = this.getBlockIdAt(x, y, z);
+				if(down){
+					y--;
+				}else{
+					y++;
+				}
+			}else{
+				id = -1;
+			}
+		}while(id==0&&id!=-1);
+		return new int[] {id,y};
+	}
+}
